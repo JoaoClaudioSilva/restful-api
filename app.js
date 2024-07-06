98f88ad9f431c56b1ac6274811a06f362a0f0d26
JoaoClaudioSilva <143845972+JoaoClaudioSilva@users.noreply.github.com>
2024-07-06 00:06:57 -0300

    final

--- app.js
+++ app.js
@@ -14,12 +14,15 @@ app.get('/', (req, res) => {
 })
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(require("./docs/swagger_doc.json", {})))
+app.use("/docs", 
+  swaggerUi.serve, 
+  swaggerUi.setup(require("./docs/swagger_doc.json"),
+  { customCssUrl: '/custom.css' }))
 
 app.use("/users", require("./routes/route_usuario"))
 app.use("/admins", require("./routes/route_admin"))
